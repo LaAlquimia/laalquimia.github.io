3e869de5acfe33cdfd00d92179d7827268a13eff
laalquimia <laalquimia@skynet.localdomain>
2023-09-13 06:37:10 -0500

    add

--- auction/js/scripts.js
+++ auction/js/scripts.js
@@ -5,3 +5,59 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+const loginWithEth = async () => {
+    // check if there is global window.web3 instance
+    if (window.web3) {
+      try {
+        // get the user's ethereum account - prompts metamask to login
+        const selectedAccount = await window.ethereum
+          .request({
+            method: "eth_requestAccounts",
+          })
+          .then((accounts) => accounts[0])
+          .catch(() => {
+            // if the user cancels the login prompt
+            throw Error("Please select an account");
+          });
+  
+        // set the global userWalletAddress variable to selected account
+        window.userWalletAddress = selectedAccount;
+  
+        // store the user's wallet address in local storage
+        window.localStorage.setItem("userWalletAddress", selectedAccount);
+  
+        const walletid = document.getElementById("walletid")
+        const tokenbalance = document.getElementById("tokenbalance") 
+  
+  
+        const conn = new Web3(window.ethereum);
+        
+  
+        const tokenContract = new conn.eth.Contract(fullABI, contractAddress);      
+        const address = ethereum.selectedAddress;
+        console.log(address)
+  
+        async function obtenerBalance() {
+          const balance = await tokenContract.methods.balanceOf(address).call()/10**18;
+          console.log(balance)
+          return balance
+        } 
+  
+        async function ogtBalance() {
+          const balance = await ogtContract.methods.balanceOf(address).call()/10**18;
+          console.log(balance);
+          return balance
+        } 
+  
+        walletid.innerHTML = address.slice(0, 3)+ "..."+address.slice(- 4);
+        bal =  await obtenerBalance();
+        tokenbalance.textContent = bal;
+  
+      } catch (error) {
+        alert(error);
+      }
+    } else {
+      alert("wallet not found");
+    }
+  };
