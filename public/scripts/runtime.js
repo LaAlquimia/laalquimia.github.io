ba63497896cad6691239bf82e7075e363e024b80
lq <lq@customer.lan>
2024-03-14 18:36:09 -0500

    🟢 Alert

--- public/scripts/runtime.js
+++ public/scripts/runtime.js
@@ -23,13 +23,13 @@ const signals = (kLine, symbol, emaDist) => {
   if (rsi[0] < rsi[1] && rsi[1] > 75 && emaDist > 3) {
     const percent = (tickers.find(ticker => ticker.symbol === symbol).price24hPcnt*100).toFixed(2);
     const signalMessage = 'SHORT ⛔'+ symbol + '\nEMA distance: ' + emaDist.toFixed(2) + '%\n 24h PriceChange ' + (tickers.find(ticker => ticker.symbol === symbol).price24hPcnt*100).toFixed(2) +'%';
-    const speech = 'Short'+ symbol + 'Distancia a la media' + emaDist.toFixed(2) + '%';
+    const speech = 'Alquialerta: Short'+ symbol + 'Distancia a la media' + emaDist.toFixed(2) + '%';
     speak(speech); 
     notifyMe(symbol, signalMessage);
   } else if (rsi[0] > rsi[1] && rsi[1] < 25 && emaDist < -3) {
     const percent = (tickers.find(ticker => ticker.symbol === symbol).price24hPcnt*100).toFixed(2);
     const signalMessage = 'LONG 🟢'+ symbol + '\nEMA distance ' + emaDist.toFixed(2) + '%\n24h PriceChange ' + percent;
-    const speech = 'long'+ symbol + 'Distancia a la media' + emaDist.toFixed(2) + '%';
+    const speech = 'Alquialerta long'+ symbol + 'Distancia a la media' + emaDist.toFixed(2) + '%';
     speak(speech);   
     notifyMe(symbol, signalMessage);
   }
