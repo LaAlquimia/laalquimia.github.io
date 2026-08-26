cf24f3783e6bcb86ea02c7a2057dac02eabb47d2
La Alquimia <laalquimia@icloud.com>
2026-08-26 15:15:35 -0500

    style: update King piece SVG with regal Staunton silhouette

--- public/chess/js/pieces.js
+++ public/chess/js/pieces.js
@@ -107,17 +107,19 @@
     // White King
     'K': `<g fill="none" fill-rule="evenodd" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
       <path d="M 22.5,11.63 L 22.5,6 M 20,8 L 25,8" stroke-linejoin="miter"/>
-      <path d="M 22.5,25 C 22.5,25 27,17.5 25.5,14.5 C 24,11.5 21,11.5 22.5,25 z" fill="#ffffff" stroke-linecap="butt" stroke-linejoin="miter"/>
-      <path d="M 11.5,37 C 17,40.5 28,40.5 33.5,37 C 36.5,34 36.5,28.5 36.5,28.5 C 36.5,28.5 38.5,24 35.5,18.5 C 32.5,13 29.5,14.5 29.5,14.5 C 29.5,14.5 28,11.5 22.5,11.5 C 17,11.5 15.5,14.5 15.5,14.5 C 15.5,14.5 12.5,13 9.5,18.5 C 6.5,24 8.5,28.5 8.5,28.5 C 8.5,28.5 8.5,34 11.5,37 z" fill="#ffffff"/>
-      <path d="M 11.5,30 C 17,27 28,27 33.5,30 M 11.5,33.5 C 17,30.5 28,30.5 33.5,33.5 M 11.5,37 C 17,34 28,34 33.5,37"/>
+      <path d="M 22.5,25 C 22.5,25 27,17.5 25.5,14.5 C 24.5,12 22.5,12 22.5,12 C 20.5,12 19.5,14.5 19.5,14.5 C 18,17.5 22.5,25 22.5,25" fill="#ffffff" stroke-linecap="butt" stroke-linejoin="miter"/>
+      <path d="M 12.5,37 C 18,40.5 27,40.5 32.5,37 L 32.5,30 C 32.5,30 41.5,25.5 38.5,19.5 C 34.5,13 25,16 22.5,23.5 L 22.5,27 L 22.5,23.5 C 20,16 10.5,13 6.5,19.5 C 3.5,25.5 12.5,30 12.5,30 L 12.5,37 z" fill="#ffffff"/>
+      <path d="M 12.5,30 C 18,27 27,27 32.5,30 M 12.5,33.5 C 18,30.5 27,30.5 32.5,33.5 M 12.5,37 C 18,34 27,34 32.5,37"/>
     </g>`,
 
     // Black King
     'k': `<g fill="none" fill-rule="evenodd" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
       <path d="M 22.5,11.63 L 22.5,6 M 20,8 L 25,8" stroke-linejoin="miter"/>
-      <path d="M 22.5,25 C 22.5,25 27,17.5 25.5,14.5 C 24,11.5 21,11.5 22.5,25 z" fill="#1e1e1e" stroke-linecap="butt" stroke-linejoin="miter"/>
-      <path d="M 11.5,37 C 17,40.5 28,40.5 33.5,37 C 36.5,34 36.5,28.5 36.5,28.5 C 36.5,28.5 38.5,24 35.5,18.5 C 32.5,13 29.5,14.5 29.5,14.5 C 29.5,14.5 28,11.5 22.5,11.5 C 17,11.5 15.5,14.5 15.5,14.5 C 15.5,14.5 12.5,13 9.5,18.5 C 6.5,24 8.5,28.5 8.5,28.5 C 8.5,28.5 8.5,34 11.5,37 z" fill="#1e1e1e"/>
-      <path d="M 20,8 L 25,8 M 22.5,6 L 22.5,11.63 M 11.5,30 C 17,27 28,27 33.5,30 M 11.5,33.5 C 17,30.5 28,30.5 33.5,33.5 M 11.5,37 C 17,34 28,34 33.5,37" stroke="#ececec"/>
+      <path d="M 22.5,25 C 22.5,25 27,17.5 25.5,14.5 C 24.5,12 22.5,12 22.5,12 C 20.5,12 19.5,14.5 19.5,14.5 C 18,17.5 22.5,25 22.5,25" fill="#1e1e1e" stroke-linecap="butt" stroke-linejoin="miter"/>
+      <path d="M 12.5,37 C 18,40.5 27,40.5 32.5,37 L 32.5,30 C 32.5,30 41.5,25.5 38.5,19.5 C 34.5,13 25,16 22.5,23.5 L 22.5,27 L 22.5,23.5 C 20,16 10.5,13 6.5,19.5 C 3.5,25.5 12.5,30 12.5,30 L 12.5,37 z" fill="#1e1e1e"/>
+      <path d="M 20,8 L 25,8 M 22.5,6 L 22.5,11.63" stroke="#ececec" stroke-linejoin="miter"/>
+      <path d="M 32,29.5 C 32,29.5 40.5,25.5 38.03,19.85 C 34.15,14 25,18 22.5,24.5 L 22.5,26.6 L 22.5,24.5 C 20,18 10.85,14 6.97,19.85 C 4.5,25.5 13,29.5 13,29.5" stroke="#ececec"/>
+      <path d="M 12.5,30 C 18,27 27,27 32.5,30 M 12.5,33.5 C 18,30.5 27,30.5 32.5,33.5 M 12.5,37 C 18,34 27,34 32.5,37" stroke="#ececec"/>
     </g>`
   };
 
@@ -617,6 +619,10 @@
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = ChessPieces;
   }
-  global.ChessPieces = ChessPieces;
+  if (typeof window !== 'undefined') {
+    window.ChessPieces = ChessPieces;
+  } else if (typeof globalThis !== 'undefined') {
+    globalThis.ChessPieces = ChessPieces;
+  }
 
-})(typeof window !== 'undefined' ? window : this);
+})(typeof window !== 'undefined' ? window : (typeof globalThis !== 'undefined' ? globalThis : this));
